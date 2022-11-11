822506ac689c5a19f3ae426b1a3e5e8c9dcaf10d
Cyprien Siaud <74303569+CSIAUD@users.noreply.github.com>
2022-11-11 23:10:55 +0100

    :bug: fix isLink return

--- api/controllers/spotify.js
+++ api/controllers/spotify.js
@@ -1,7 +1,10 @@
 // 📚 Librairies
 const fs = require('fs');
-const { writeFile, readFile } = require('fs');
+const {readFile} = require('fs/promises');
+// const { writeFile, readFile } = require('fs');
+const { writeFile } = require('fs');
 var request = require('request'); // "Request" library
+const file = '../api/data/users.json';
 
 var client_id = process.env.CLIENT_ID;
 var client_secret = process.env.CLIENT_SECRET;
@@ -29,36 +32,24 @@ module.exports.refreshToken = (req, res) => {
 }
 
 module.exports.link = async (req, res) => {
-    isLinked(req.body.username)
-    .then((data) => {
-        console.log(data)
-        // if(linked){
-        //     console.log("yes");
-        //     res.send("Compte déjà lié à Spotify");
-        // } else {
-        //     console.log("nope");
-        //     res.send("Lien en cours");
-        // }
-        res.send();
-    })
+    if (await isLinked(req.body.username)) {
+        res.send("Compte déjà lié à Spotify");
+    } else {
+        res.send("Lien en cours");
+    }
 }
 
 async function isLinked(username) {
     try {
-        const file = '../api/data/users.json';
-        readFile(file, (err, data) => {
-            if (err) {
-                console.log("Erreur : ", err);
-                return false;
-            }
+        return await readFile(file, 'utf-8')
+        .then((data) => {
             const parsedData = JSON.parse(data);
             let users = parsedData.users;
             if(!users.length) return false;
-
+            // ==============================
             const user = users.find(u => u.username.toLowerCase() === username.toLowerCase());
             if(!user.link) return false;
-            console.log("link");
-
+            // ==============================
             if(user.link.length != 2){
                 let index = users.indexOf(user);
                 delete parsedData.users[index].link
@@ -66,14 +57,14 @@ async function isLinked(username) {
                 writeFile(file, JSON.stringify(parsedData, null, 2), (err) => {
                     if (err) {
                         console.log("Une erreur est survenue lors de la mise à jour du fichier users.json.");
+                        return;
                     }
                     console.log("Le lien a été supprimé");
                 });
     
                 return false;
             } 
-            console.log("length");
-
+            // ==============================
             if(user.link[0] == "" || user.link[1] == ""){
                 let index = users.indexOf(user);
                 delete parsedData.users[index].link
@@ -81,19 +72,19 @@ async function isLinked(username) {
                 writeFile(file, JSON.stringify(parsedData, null, 2), (err) => {
                     if (err) {
                         console.log("Une erreur est survenue lors de la mise à jour du fichier users.json.");
+                        return;
                     }
                     console.log("Le lien a été supprimé");
                 });
 
                 return false;
             }
-            console.log("content");
-
+            // ==============================
             return true;
-        });
+        })
     } catch(err) {
         console.log(err);
-        return false;
+        throw 'Unable to search users list.'
     }
 }
 
